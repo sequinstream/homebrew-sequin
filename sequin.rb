eac0fe7e0da04c8c63e7d3a62099b6072c20fd92
RTLS <carter.e.pedersen@gmail.com>
2024-06-28 14:50:48 -0700

    🎉 Initial commit

---
+++ sequin.rb
@@ -0,0 +1,16 @@
+class Sequin < Formula
+  desc "CLI tool for sequin"
+  homepage "https://github.com/sequin-io/sequin-cli"
+  url "https://github.com/sequin-io/sequin-cli.git", tag: "v0.1.0"
+  head "https://github.com/sequin-io/sequin-cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(output: bin/"sequin")
+  end
+
+  test do
+    assert_match "sequin", shell_output("#{bin}/sequin --help")
+  end
+end
