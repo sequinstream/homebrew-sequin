cf496adfeb14bb3dd9cc41d54618cf85a4cd2ed6
RTLS <carter.e.pedersen@gmail.com>
2024-07-24 17:29:35 -0700

    remove extra v

--- sequin.rb
+++ sequin.rb
@@ -6,20 +6,20 @@ class Sequin < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sequinstream/sequin/releases/download/v#{version}/sequin-cli-#{version}-darwin-arm64.zip"
+      url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-darwin-arm64.zip"
       sha256 "8d4a7497ac8f9e680183afaf9c73ffc83984cba4e62c88b40fef07cfd7d2a4fc"
     else
-      url "https://github.com/sequinstream/sequin/releases/download/v#{version}/sequin-cli-#{version}-darwin-amd64.zip"
+      url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-darwin-amd64.zip"
       sha256 "8d4a7497ac8f9e680183afaf9c73ffc83984cba4e62c88b40fef07cfd7d2a4fc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sequinstream/sequin/releases/download/v#{version}/sequin-cli-#{version}-linux-arm64.zip"
+      url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-linux-arm64.zip"
       sha256 "8d4a7497ac8f9e680183afaf9c73ffc83984cba4e62c88b40fef07cfd7d2a4fc"
     else
-      url "https://github.com/sequinstream/sequin/releases/download/v#{version}/sequin-cli-#{version}-linux-amd64.zip"
+      url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-linux-amd64.zip"
       sha256 "8d4a7497ac8f9e680183afaf9c73ffc83984cba4e62c88b40fef07cfd7d2a4fc"
     end
   end
