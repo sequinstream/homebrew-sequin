459e69300dfa57023d1e4db0aa2162906901c2e6
RTLS <carter.e.pedersen@gmail.com>
2025-03-25 11:57:30 -0700

    Bump sequin-cli version to v0.6.75 and update SHA256 checksums

--- sequin.rb
+++ sequin.rb
@@ -1,26 +1,26 @@
 class Sequin < Formula
   desc "CLI tool for sequin"
   homepage "https://github.com/sequinstream/sequin"
-  version "v0.6.74"
+  version "v0.6.75"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-darwin-arm64.zip"
-      sha256 "c5586d980adf85d6664f59ef03b178ce43cfdd852843a90de9a8badd63967967" # tag:darwin-arm64
+      sha256 "932d9b65645272b33e4166b458409c00c907b4c00bfc95db389d89f90084fb78" # tag:darwin-arm64
     else
       url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-darwin-amd64.zip"
-      sha256 "dd29989e8cc20ccf3bf1d1861cfb1c7923f4ea3800b19fb29b511a0a62354687" # tag:darwin-amd64
+      sha256 "407e6be65984d6be8e25367c9ba13387e0c9d72593c61ae8ee84cf2c94b0c1ae" # tag:darwin-amd64
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-linux-arm64.zip"
-      sha256 "16734cefc3bfdc2aef4f24db3f0e07f4f530a3cab3d9e91ba2391b2bcd82c432" # tag:linux-arm64
+      sha256 "fc315b363546542e4da43927e32972aef7bf78432dd2f2060c21d9240b126d21" # tag:linux-arm64
     else
       url "https://github.com/sequinstream/sequin/releases/download/#{version}/sequin-cli-#{version}-linux-amd64.zip"
-      sha256 "995c04b56a7aeabb0d728f17b1258fa3448bd7834e1f500dcad9aa3c6d60c721" # tag:linux-amd64
+      sha256 "6bb25aef08a5c8cecacf6da5de9566deca90161a2c3b5151cc9f008974791ef6" # tag:linux-amd64
     end
   end
 
